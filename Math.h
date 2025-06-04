e882a60dff7c975c96cf39f070839d8040947956
Neo7k <neo7k@ya.ru>
2025-06-04 02:20:26 +0200

    Perspective

--- Math.h
+++ Math.h
@@ -31,9 +31,14 @@ struct v4
 		: x(in_x), y(in_y), z(in_z), w(in_w) {}
 	v4(float in_x, float in_y)
 		: x(in_x), y(in_y), z(0.0f), w(1.0f) {}
+	v4(float in_x, float in_y, float in_z)
+		: x(in_x), y(in_y), z(in_z), w(1.0f) {}
 	
-	v4 operator * (i2 other) { return v4{x * other.x, y * other.y, z, w};	}
-	v4 operator * (v4 other) { return v4{x * other.x, y * other.y, z * other.z, w * other.w}; }
+	v4 operator * (i2 other) const { return v4{x * other.x, y * other.y, z, w};	}
+	v4 operator * (v4 other) const { return v4{x * other.x, y * other.y, z * other.z, w * other.w}; }
+	v4 operator * (float other) const { return v4{x * other, y * other, z * other, w * other}; }
+	v4 operator += (v4 other) { x += other.x; y += other.y; z += other.z; w += other.w; return *this; }
+	v4 operator + (v4 other) const { return v4{x + other.x, y + other.y, z + other.z, w + other.w}; }
 
 	static v4 Zero()
 	{
@@ -45,6 +50,11 @@ struct v4
 		return x * v.x + y * v.y + z * v.z + w * v.w;
 	}
 
+	v4 Normalized() const
+	{
+		return *this * (1.0f / sqrt(Dot(*this)));
+	}
+
 };
 
 struct b4
@@ -54,6 +64,14 @@ struct b4
 	uchar z;
 	uchar w;
 
+	b4() = default;
+	b4(uchar value) : x(value), y(value), z(value), w(value) {}
+	b4(uchar in_x, uchar in_y, uchar in_z, uchar in_w) : x(in_x), y(in_y), z(in_z), w(in_z) {}
+	b4(v4 in) 
+		: x((uchar)std::clamp(in.x, 0.0f, 255.0f))
+		, y((uchar)std::clamp(in.y, 0.0f, 255.0f))
+		, z((uchar)std::clamp(in.z, 0.0f, 255.0f))
+		, w((uchar)std::clamp(in.w, 0.0f, 255.0f)) {}
 	void operator += (b4 other) { x += other.x; y += other.y; z += other.z; w += other.w; }
 };
 
@@ -72,6 +90,11 @@ i2 Clamp(i2 v, i2 min, i2 max)
 	return {std::clamp(v.x, min.x, max.x), std::clamp(v.y, min.y, max.y)};
 }
 
+b4 Clamp(b4 v, uchar min, uchar max)
+{
+	return {std::clamp(v.x, min, max), std::clamp(v.y, min, max), std::clamp(v.z, min, max), std::clamp(v.w, min, max)};
+}
+
 struct Mtx
 {
 	Mtx() = default;
@@ -88,6 +111,13 @@ struct Mtx
 		lines[1] = l2;
 		lines[2] = l3;
 		lines[3] = v4{0.0f, 0.0f};
+	}
+	Mtx(v4 l1, v4 l2, v4 l3, v4 l4)
+	{
+		lines[0] = l1;
+		lines[1] = l2;
+		lines[2] = l3;
+		lines[3] = l4;
 	}
 
 	v4 operator * (v4 v) const
--- StoneAge.cpp
+++ StoneAge.cpp
@@ -23,6 +23,12 @@ struct Image
 
 struct Quad
 {
+	Quad() = default;
+	Quad(v4 vert1, v4 vert2, v4 vert3, v4 vert4)
+		: verts{vert1, vert2, vert3, vert4} {}
+	Quad(v4 vert1, v4 vert2, v4 vert3, v4 vert4, v4 in_color)
+		: verts{vert1, vert2, vert3, vert4}, color(in_color) {}
+
 	v4 GetMin() const
 	{
 		v4 m = verts[0];
@@ -57,60 +63,107 @@ struct Quad
 		return Quad{m * q.verts[0],
 								m * q.verts[1],
 								m * q.verts[2],
-								m * q.verts[3]};
+								m * q.verts[3],
+								q.color};
 	}
 
 	v4 verts[4];
+	v4 color{1.0f, 0.0f};
 };
 
 int main()
 {
-	Image<{32, 32}> frame;
+	Image<{128, 128}> frame;
 	frame.Clear();
 
-	GameWindow window(frame, 32);
+	GameWindow window(frame, 8);
+
+	std::vector<Quad> quads;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		quads.emplace_back(Quad
+		{ 
+			v4{-0.75f + i * 0.5f, 0.5f, 1.0f},
+			v4{-0.75f + i * 0.5f, -0.5f, 1.0f},
+			v4{-0.75f + i * 0.5f, -0.5f, 10.0f},
+			v4{-0.75f + i * 0.5f, 0.5f, 10.0f},
+			v4{0.0f, 0.0f, 1.0f}
+		});
+	}
+
+	for (int i = 0; i < 2; ++i)
+	{
+		quads.emplace_back(Quad
+		{
+			v4{-0.75f + i, 0.5f, 1.0f},
+			v4{-0.75f + i, -0.5f, 1.0f},
+			v4{-0.25f + i, -0.5f, 1.0f},
+			v4{-0.25f + i, 0.5f, 1.0f},
+			v4{1.0f, 0.0f, 0.0f}
+		});
+	}
 
 	auto&& frame_fn =
 		[&]
 		()
 		{
 			frame.Clear();
-			Quad q{ v4{0.45f, 0.25f}, v4{0.25f, 0.75f},
-							v4{0.75f, 0.55f}, v4{0.95f, 0.05f}};
-
-			static float alpha = 0.0f;
-			alpha += 0.02f;
-			Mtx rotation{	v4{cos(alpha), 	0.0f, sin(alpha), 0.0f},
-										v4{0.0f, 				1.0f, 0.0f, 			0.0f},
-										v4{-sin(alpha), 0.0f, cos(alpha), 0.0f}};
-			q = rotation * q;
-
-			Mtx to_screen_ctr{v4{0.5f, 0.0f, 0.0f, 0.5f},
-												v4{0.0f, 0.5f, 0.0f, 0.5f}};
-			q = to_screen_ctr * q;
-
-			i2 top_left = Floor_i2(q.GetMin() * frame.GetSize());
-			i2 bottom_right = Ceil_i2(q.GetMax() * frame.GetSize());
-			top_left = Clamp(top_left, {0, 0}, frame.GetSize());
-			bottom_right = Clamp(bottom_right, {0, 0}, frame.GetSize());
-			v4 frame_size_inv{1.0f / frame.GetSize().x, 1.0f / frame.GetSize().y};
-			for (int y = top_left.y; y < bottom_right.y; ++y)
-				for (int x = top_left.x; x < bottom_right.x; ++x)
+			static float t = 0.0f;
+			t += 0.005;
+			for (Quad q : quads)
+			{
+				Mtx projection 
 				{
-					v4 p = v4{x + 0.5f, y + 0.5f} * frame_size_inv; 
-					float e[4] {q.GetEdgeValue(p, 0),
-											q.GetEdgeValue(p, 1),
-											q.GetEdgeValue(p, 2),
-											q.GetEdgeValue(p, 3)};
-					if (e[0] > 0 && e[1] > 0 && e[2] > 0 && e[3] > 0)
+					v4{1.0f, 0.0f, 0.0f, 0.0f},
+					v4{0.0f, -1.0f, 0.0f, 0.0f},
+					v4{0.0f, 0.0f, 1.0f, 0.0f},
+					v4{0.0f, 0.0f, std::clamp(t, 0.0f, 1.0f), std::clamp(1.0f - t, 0.0f, 1.0f)}
+				};
+				q = projection * q;
+				for (v4& vert : q.verts)
+					if (vert.w != 0.0f)
+						vert = vert * (1.0f / vert.w); 
+
+				Mtx to_screen_ctr
+				{
+					v4{0.5f, 0.0f, 0.0f, 0.5f},
+					v4{0.0f, 0.5f, 0.0f, 0.5f}
+				};
+				q = to_screen_ctr * q;
+
+				i2 top_left = Floor_i2(q.GetMin() * frame.GetSize());
+				i2 bottom_right = Ceil_i2(q.GetMax() * frame.GetSize());
+				top_left = Clamp(top_left, {0, 0}, frame.GetSize());
+				bottom_right = Clamp(bottom_right, {0, 0}, frame.GetSize());
+				v4 frame_size_inv{1.0f / frame.GetSize().x, 1.0f / frame.GetSize().y};
+				for (int y = top_left.y; y < bottom_right.y; ++y)
+					for (int x = top_left.x; x < bottom_right.x; ++x)
 					{
-						frame.Set({x, y}, {255, 0, 0, 0}); 
+						const float ms[][2] = {{0.05f, 0.25f}, {0.45f, 0.05f}, {0.55f, 0.95f}, {0.95f, 0.75f}};
+						v4 pixel_color = v4::Zero();
+						for (int i = 0; i < 4; ++i)
+						{
+							v4 p = v4{x + ms[i][0], y + ms[i][1]} * frame_size_inv; 
+							float e[4] {q.GetEdgeValue(p, 0),
+													q.GetEdgeValue(p, 1),
+													q.GetEdgeValue(p, 2),
+													q.GetEdgeValue(p, 3)};
+							if (e[0] > 0 && e[1] > 0 && e[2] > 0 && e[3] > 0)
+							{
+								pixel_color = v4{q.color.x, q.color.y, q.color.z, pixel_color.w + 0.25f};
+							}
+							else if (e[0] < 0 && e[1] < 0 && e[2] < 0 && e[3] < 0)
+							{
+								pixel_color = v4{q.color.x, q.color.y, q.color.z, pixel_color.w + 0.25f};
+							}
+						}
+						b4 fp = frame.pix[y][x];
+						v4 frame_pixel = v4{(float)fp.x, (float)fp.y, (float)fp.z, (float)fp.w} * (1.0f / 255.0f);
+						frame_pixel = pixel_color * pixel_color.w + frame_pixel * (1.0f - pixel_color.w);
+						frame.Set({x, y}, b4(frame_pixel * 255.0f));
 					}
-					else if (e[0] < 0 && e[1] < 0 && e[2] < 0 && e[3] < 0)
-					{
-						frame.Set({x, y}, {0, 0, 255, 0});
-					}
-				}
+			}
 		};
 	
 	window.Run(frame_fn);
